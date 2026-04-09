9d1ec331a6f9d8b18cbd9b04d46c445514445082
Egor Danilov <159701362+kimmito@users.noreply.github.com>
2026-04-09 22:55:50 +0300

    refactor(Grid): update grid adaptivity

--- src/app/components/ui/CatGrid.tsx
+++ src/app/components/ui/CatGrid.tsx
@@ -10,7 +10,7 @@ interface ICatGridProps {
 
 export const CatGrid: FC<ICatGridProps> = ({ cats, favoriteIds, onToggleFavorite }) => {
   return (
-    <div className="grid justify-center gap-x-6 gap-y-8 grid-cols-[14.0625rem] sm:grid-cols-[repeat(2,14.0625rem)] md:grid-cols-[repeat(3,14.0625rem)] lg:grid-cols-[repeat(5,14.0625rem)] lg:gap-x-12 lg:gap-y-13">
+    <div className="grid justify-center gap-x-6 gap-y-8 grid-cols-[14.0625rem] sm:grid-cols-[repeat(2,14.0625rem)] md:grid-cols-[repeat(3,14.0625rem)] lg:max-[1440px]:grid-cols-[repeat(4,14.0625rem)] lg:grid-cols-[repeat(5,14.0625rem)] lg:gap-x-12 lg:gap-y-13">
       {cats.map((cat) => (
         <CatCard
           key={cat.id}
